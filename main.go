82dc226e3c81485d2664fd6d85481014b91ee70d
Chelsea H. Komlo <chelsea.komlo@gmail.com>
2017-03-12 22:32:29 -0500

    use new api

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	if len(os.Args) > 1 {
 		message := os.Args[1]
 		kp := lamport.GenLamportKeyPair()
-		l := lamport.GenSignature(message, kp)
+		l := lamport.Sign(message, kp)
 		fmt.Printf("Your signature is: %s \n", l)
 	} else {
 		fmt.Println("Please pass in a message to be signed!")
